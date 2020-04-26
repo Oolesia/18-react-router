bc99addedfb5cd324b4a03bb55465dd3e3726245
Olesia Sikora <sikora0310@gmail.com>
2020-04-26 23:21:53 +0300

    Updates

---
+++ precache-manifest.5377276c4a9d14e8efe26169a91c7f11.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed1fc69786cb4872ade31922e14b162",
+    "url": "/18-react-router/index.html"
+  },
+  {
+    "revision": "01a0aa35dec3396fb8dd",
+    "url": "/18-react-router/static/css/main.143dd1c5.chunk.css"
+  },
+  {
+    "revision": "5adf3adc58b793b335b0",
+    "url": "/18-react-router/static/js/2.1814c61d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/18-react-router/static/js/2.1814c61d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a0aa35dec3396fb8dd",
+    "url": "/18-react-router/static/js/main.5739839e.chunk.js"
+  },
+  {
+    "revision": "0d64717329b034f47db2",
+    "url": "/18-react-router/static/js/runtime-main.bab578bc.js"
+  },
+  {
+    "revision": "5c87994c964d115c484b909be298743b",
+    "url": "/18-react-router/static/media/female.5c87994c.svg"
+  },
+  {
+    "revision": "2d4ffb58d613f039252623e41007a1c5",
+    "url": "/18-react-router/static/media/hydrangea.2d4ffb58.jpg"
+  },
+  {
+    "revision": "075829956d01b4f9d072ca02aa84e2f2",
+    "url": "/18-react-router/static/media/male.07582995.svg"
+  },
+  {
+    "revision": "984f872adb7ab489a93068275c3b6765",
+    "url": "/18-react-router/static/media/nature-1.984f872a.jpg"
+  },
+  {
+    "revision": "40fd6a115ed2ef880daf6b99ed28812e",
+    "url": "/18-react-router/static/media/nature-2.40fd6a11.jpg"
+  },
+  {
+    "revision": "387b1130c5a454ae67cc6229996cdb01",
+    "url": "/18-react-router/static/media/nature-3.387b1130.jpg"
+  },
+  {
+    "revision": "e72f0feecad84012c25e0637b0cbe8f0",
+    "url": "/18-react-router/static/media/nature-4.e72f0fee.jpg"
+  },
+  {
+    "revision": "363613fb1a629e41566030b86ca3781a",
+    "url": "/18-react-router/static/media/nature-5.363613fb.jpg"
+  },
+  {
+    "revision": "4eb878dec452bcb261228679eb65f3b2",
+    "url": "/18-react-router/static/media/nature-6.4eb878de.jpg"
+  }
+]);
